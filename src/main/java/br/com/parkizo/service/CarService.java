b0a8b67d23b15ad215addc8f29d0d3657579d491
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-24 13:13:01 -0300

    feat: Cria novas funcionalidades na camada de Service do Car

--- src/main/java/br/com/parkizo/service/CarService.java
+++ src/main/java/br/com/parkizo/service/CarService.java
@@ -3,9 +3,14 @@ package br.com.parkizo.service;
 import br.com.parkizo.entity.Car;
 import br.com.parkizo.repository.CarRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.hibernate.grammars.hql.HqlParser;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +22,21 @@ public class CarService {
         return carRepository.findAll();
     }
 
+    public Optional<Car> getById(Long id) {
+        return carRepository.findById(id);
+    }
+
     public Car createPark(Car car) {
         return carRepository.save(car);
+    }
+
+    public String finishPark(Long id) {
+        Optional<Car> car = carRepository.findById(id);
+        if(car.isPresent()){
+            carRepository.deleteById(id);
+            return "Park finished with success";
+        }
+        return "Car with Id: " + id + " doesn't exists";
     }
 
 }
